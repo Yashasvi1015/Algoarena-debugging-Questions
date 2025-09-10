f2388f90ab7230be837b355db5548b15e6314113
Yashasvi1015 <yashasvisharma846@gmail.com>
2025-09-10 12:19:39 +0530

    Create question10.cpp

---
+++ question10.cpp
@@ -0,0 +1,24 @@
+/*
+The function/method sumDigits is intended to return the sum of digits of a given integer n.  
+The code compiles but fails to produce the correct result due to incomplete loop logic.  
+
+Example:
+Input: 123
+Output: 6
+*/
+
+#include <iostream>
+using namespace std;
+
+int sumDigits(int n) {
+    int sum = 0;
+    while (n > 0) {
+     return sum;
+    }   
+}
+
+int main() {
+    int n = 765;
+    cout << sumDigits(n) << endl;
+    return 0;
+}
