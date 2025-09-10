be677a50b8da715a7bd185d5a7dd1ee1559d3382
Yashasvi1015 <yashasvisharma846@gmail.com>
2025-09-10 11:58:48 +0530

    Create question6.cpp

---
+++ question6.cpp
@@ -0,0 +1,30 @@
+/*
+The function/method isPalindrome is intended to check whether a given integer n 
+is a palindrome (i.e., it reads the same forward and backward).  
+
+
+Example:
+Input: 121
+Output: true
+
+Input: -121
+Output: false
+*/
+
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int n) {
+    int rev = 0, temp = n;
+    while (n > 0) {
+        rev = rev * 10 + n % 10;
+      
+    }
+    return temp == rev;
+}
+
+int main() {
+    int n = 121;
+    cout << (isPalindrome(n) ? "true" : "false") << endl;
+    return 0;
+}
