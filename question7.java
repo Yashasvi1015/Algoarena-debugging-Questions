375970688be946e7a510ef39fb865d7e1920bdb0
Yashasvi1015 <yashasvisharma846@gmail.com>
2025-09-10 20:08:09 +0530

    question7.java

---
+++ question7.java
@@ -0,0 +1,21 @@
+/*
+The function/method countDigits is intended to return the number of 
+digits in a given positive integer n.  
+Example:
+Input: 12345
+Output: 5
+*/
+
+public class CountDigits {
+    static int countDigits(int n) {
+        int count = 0;
+        while (n > 0) {
+
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int n = 6745321;
+        System.out.println(countDigits(n));
+    }
+}
