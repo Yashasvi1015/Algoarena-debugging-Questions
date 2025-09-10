9c9b8b467c614f20cdd7327c780b74cb6c82be1d
Yashasvi1015 <yashasvisharma846@gmail.com>
2025-09-10 20:03:37 +0530

    question4.java

---
+++ question4.java
@@ -0,0 +1,21 @@
+/*
+The function/method factorial is intended to return the factorial of a given integer n using recursion.
+The function compiles successfully but fails to return the correct result for some test cases 
+because the recursive step is missing.
+
+Your task is to fix the code so that it passes all the test cases.
+*/
+
+public class Factorial {
+    static long factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+
+        }
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(factorial(n));
+    }
+}
