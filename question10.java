b6454c32efe179a0c0d3b1dcf4e2d19c0bfc2859
Yashasvi1015 <yashasvisharma846@gmail.com>
2025-09-10 20:11:21 +0530

    question10.java

---
+++ question10.java
@@ -0,0 +1,22 @@
+/*
+The function/method sumDigits is intended to return the sum of digits of a given integer n.  
+The code compiles but fails to produce the correct result due to incomplete loop logic.  
+
+Example:
+Input: 123
+Output: 6
+*/
+
+public class SumDigits {
+    	static int sumDigits(int n) {
+       	 int sum = 0;
+       	 while (n > 0) {
+           	 return sum;
+       	 }
+    }
+    public static void main(String[] args) {
+        int n = 765;
+        System.out.println(sumDigits(n));
+          }
+     }
+}
