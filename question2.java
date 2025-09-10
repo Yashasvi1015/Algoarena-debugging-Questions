9aecc6aae802e5b7d199fb1bc97426e90aee9882
Yashasvi1015 <yashasvisharma846@gmail.com>
2025-09-10 19:58:52 +0530

    question2.java

---
+++ question2.java
@@ -0,0 +1,22 @@
+/*
+    DEBUGGING QUESTION:
+
+    You are given a function `fib` that accepts one argument:
+    - n, an integer representing the index of the Fibonacci sequence.
+
+    The function is supposed to return the n-th Fibonacci number.
+*/
+
+public class Fibonacci {
+    static int fib(int n) {
+        if (n < 1) return n;
+        	int prev2 = 0, prev1 = 1, cur;
+      	    for (int i = 2; i <= n; i++) {
+       	  }
+        	return cur;
+   	 }
+    	public static void main(String[] args) {
+        	int n = 5;
+        	System.out.println(fib(n));
+    	  }
+      }
